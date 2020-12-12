7443d1c09567ae5f042e8461fd3996e3943b80ef
Ridwan ONIKOYI <onikoyiridwan@gmail.com>
2020-12-11 16:29:00 -0800

    user can get minified url by slug

--- src/api/index.js
+++ src/api/index.js
@@ -4,13 +4,21 @@ import yup from 'yup';
 import Miny from '../model/miny.js';
 
 const router = express.Router();
+const slugMessage = 'slug consisting only of non-whitespaces use (Underscore and Dash Is allowed)';
 const schema = yup.object().shape({
   slug: yup.string().trim().matches(/^[a-zA-Z0-9_-]*$/i, {
-    message: 'slug consisting only of non-whitespaces use _ or -'
+    message: slugMessage
   }),
   url: yup.string().trim().url()
 });
 
+const slugSchema = yup.object().shape({
+  slug: yup.string().trim().matches(/^[a-zA-Z0-9_-]*$/i, {
+    message: slugMessage
+  }),
+});
+
+// Create a short url
 router.post('/url', async (req, res, next) => {
   try {
     let { slug } = req.body;
@@ -37,6 +45,22 @@ router.post('/url', async (req, res, next) => {
       const value = await miny.save();
       res.status(201).json({ data: value, message: 'URL minified successfully', status: 201 });
     }
+  } catch (error) {
+    next(error);
+  }
+});
+
+// GET short url dateils by Slug
+router.get('/url/:slug', async (req, res, next) => {
+  try {
+    const slug = req.params.slug.trim();
+    await slugSchema.validate({ slug });
+    const minyURL = await Miny.findOne({ slug });
+    if (!minyURL) {
+      const err = new Error('Opps!, Slug you entered is not found');
+      res.send(404).json({ message: err.message, status: 404 });
+    }
+    res.status(200).json({ message: 'Yess!, Slug is found', status: 200, data: minyURL });
   } catch (error) {
     next(error);
   }
